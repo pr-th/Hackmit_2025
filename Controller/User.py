52b1ee7e8ad31e6c8658f4f3a5dba246a0d1eaf4
pr-th <prthmhll@gmail.com>
2025-03-18 23:12:23 +0530

    skeleton ig

--- Controller/User.py
+++ Controller/User.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-user_bp = Blueprint('user', __name__) 
+user_bp = Blueprint('user', __name__,template_folder="User") 
 
 @user_bp.route('/home')
 def homepage():
--- app.py
+++ app.py
@@ -7,8 +7,20 @@ app.register_blueprint(user_bp, url_prefix='/user')
 app.register_blueprint(admin_bp, url_prefix='/admin')
 
 @app.route('/')
+@app.route('/home')
 def homepage():
-    return "home"
+    return render_template("Home.html") 
+  
+@app.route('/about')
+def about():
+    return render_template("About.html")
 
+@app.route('/login')
+def login():
+    return render_template("Login.html")   
+
+@app.route('/register')
+def register():
+    return render_template("Register.html")   
 if __name__ == "__main__":
     app.run(debug = True)
